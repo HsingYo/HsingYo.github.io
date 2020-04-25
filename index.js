a89a7041661349f1d318c3602282049ee06a1e70
HsingYo <63661414+HsingYo@users.noreply.github.com>
2020-04-26 00:01:16 +0800

    v-for

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ var app = new Vue({
   el: '#app',
   data: {
     message: 'Hello Vue!'
-    message2: 'Second'
+  }
+})
+
+var app4 = new Vue({
+  el: '#app-4',
+  data: {
+    todos: [
+      { text: 'Learn JavaScript' },
+      { text: 'Learn Vue' },
+      { text: 'Build something awesome' }
+    ]
   }
 })
